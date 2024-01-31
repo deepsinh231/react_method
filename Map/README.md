bd8e66bc56fd476cb001486b461bd1b5c982f296
Deepsinh231 <deepsinh231@gmail.com>
2024-01-31 16:46:42 +0530

    Map

--- Map/README.md
+++ Map/README.md
@@ -6,3 +6,13 @@ Currently, two official plugins are available:
 
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+
+Main File
+<h1>App.jsx</h1>
+<img src="./public/Map.png">
+<br/>
+<br/>
+<br/>
+<h1>MapE.jsx</h1>
+<img src="./public/Map1.png">
--- Map/src/App.jsx
+++ Map/src/App.jsx
@@ -1,35 +1,74 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import { MapE } from './Main_Fun/MapE.jsx'
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { atomOneDarkReasonable } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
+  const Name = [
+    { id: 1, title: "button item" },
+    { id: 2, title: "button item" },
+    { id: 3, title: "button item" },
+    { id: 4, title: "button item" },
+    { id: 5, title: "button item" },
+    { id: 6, title: "button item" },
+    { id: 7, title: "button item" },
+    { id: 8, title: "button item" },
+  ]
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="w-961 py-8">
+        <MapE of={Name} render={(item, index) =>
+          <button
+            type="button"
+            className="block w-full cursor-pointer rounded-lg p-4 text-center transition duration-500 hover:bg-neutral-100 hover:text-neutral-500 focus:bg-neutral-100 focus:text-neutral-500 focus:ring-0 dark:hover:bg-neutral-600 dark:hover:text-neutral-200 dark:focus:bg-neutral-600 dark:focus:text-neutral-200">
+            {item.id} button item
+          </button>} />
+        <SyntaxHighlighter className="rounded-5 ad" language="jsx" style={atomOneDarkReasonable}>
+          {codeString}
+        </SyntaxHighlighter>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
 
-export default App
+const codeString = `
+import React from 'react'
+import { MapE } from './Main_Fun/MapE.jsx'
+
+export default function App() {
+  const Name = [
+    { id: 1, title: "button item" },
+    { id: 2, title: "button item" },
+    { id: 3, title: "button item" },
+    { id: 4, title: "button item" },
+    { id: 5, title: "button item" },
+    { id: 6, title: "button item" },
+    { id: 7, title: "button item" },
+    { id: 8, title: "button item" },
+  ]
+  return (
+    <>
+      <div className="w-961 py-8">
+        <MapE of={Name} render={(item, index) =>
+          <button
+            type="button"
+            className="block w-full cursor-pointer rounded-lg p-4 
+            text-center transition duration-500 hover:bg-neutral-100
+            hover:text-neutral-500 focus:bg-neutral-100 focus:text-neutral-500 
+            focus:ring-0 dark:hover:bg-neutral-600
+            dark:hover:text-neutral-200
+            dark:focus:bg-neutral-600
+            dark:focus:text-neutral-200">
+            {item.id} button item
+          </button>} />
+      </div>
+    </>
+  )
+}
+
+{ MapE } from './Main_Fun/MapE.jsx'
+
+import { Children } from 'react'
+export const MapE = ({ render, of }) => Children.toArray(of.map((item, index) => render(item, index)))
+
+`
